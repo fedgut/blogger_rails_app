934d74a8b166f474df32c8ab812b4313984b4192
fedgut <fedgut@gmail.com>
2019-10-21 17:50:14 -0500

    Moved further into the tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new
+    @articles.title = params[:article][:title]
+    @articles.save
+    redirect_to article_path(@articles)
+  end
 end
